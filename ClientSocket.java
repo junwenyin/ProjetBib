ffbef53db92124a74eed54688bd7e97530e38547
junwenyin <junwen.yin@gmail.com>
2016-12-04 16:32:01 +0100

    Update ClientSocket.java

--- ClientSocket.java
+++ ClientSocket.java
@@ -43,9 +43,9 @@ public class ClientSocket {
 	/**
 	 * Proc&eacute;dure principale.
 	 */
-	public ArrayList<String> myGo(String str_Commande) throws Exception {
+	public ArrayList<String> myGo(String str_Commande,int port) throws Exception {
 		ArrayList<String> res= new  ArrayList<String>();
-		theConversation = new Socket("localhost", 5000);
+		theConversation = new Socket("localhost", port);
 		theIn = new BufferedReader(new InputStreamReader(theConversation.getInputStream()));
 		theOut = new PrintStream(new BufferedOutputStream(theConversation.getOutputStream()));
 		theOut.println(str_Commande);
@@ -79,25 +79,25 @@ public class ClientSocket {
 	public static void main(String[] args) throws Exception {
 		ArrayList<String> res;
 		a("test se loger ---------------------------------------");
-		res = theAppli.myGo("TYPE=0000;NAME=Junwen;PASS=xxxx;");
+		res = theAppli.myGo("TYPE=0000;NAME=Junwen;PASS=xxxx;",5000);
 		
 		a("test get liste ---------------------------------------");
-		res = theAppli.myGo("TYPE=0001;TOKEN=Junwen;");
+		res = theAppli.myGo("TYPE=0001;TOKEN=Junwen;",5000);
 		
 		a("test chercher avec id ---------------------------------------");
-		res = theAppli.myGo("TYPE=0002;ID=10001;TOKEN=Junwen;");
+		res = theAppli.myGo("TYPE=0002;ID=10001;TOKEN=Junwen;",5000);
 		
 		a("test chercher avec auteur ---------------------------------------");
-		res = theAppli.myGo("TYPE=0002;AUTEUR=xxx;TOKEN=Junwen;");
+		res = theAppli.myGo("TYPE=0002;AUTEUR=xxx;TOKEN=Junwen;",5000);
 		
 		a("test chercher avec name ---------------------------------------");
-		res = theAppli.myGo("TYPE=0002;NAME=math;TOKEN=Junwen;");
+		res = theAppli.myGo("TYPE=0002;NAME=math;TOKEN=Junwen;",5000);
 		
 		a("test télécharger une livre avec id ---------------------------------------");
-		res = theAppli.myGo("TYPE=0003;ID=10001;TOKEN=Junwen;");
+		res = theAppli.myGo("TYPE=0003;ID=10001;TOKEN=Junwen;",5001);
 		
 		a("test 7 wrong---------------------------------------");
-		res = theAppli.myGo("TYPE=4254;ID=10001;TOKEN=Junwen;");
+		res = theAppli.myGo("TYPE=4254;ID=10001;TOKEN=Junwen;",5000);
 	}
 }
 
